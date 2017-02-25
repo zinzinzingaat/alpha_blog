65c80c4a3c3f3b55f2e688d6c8c540ac475c0392
aniketrao92 <aniket.rao92@gmail.com>
2017-02-25 04:47:49 +0000

    Created articles table, model and added validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
+=begin 
+Check Validations
+To check errors
+articles.errors.any?
+articles.errors.full_messages
+=end
 class Article < ActiveRecord::Base
-
-
+  validates :title, presence: true, length: {minimum: 3 , maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
